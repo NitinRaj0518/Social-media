28358c432edb6b1d97ed396ad601e5f0a7656447
NitinRaj0518 <129197313+NitinRaj0518@users.noreply.github.com>
2023-11-26 15:58:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://nitinraj:mongonitin123@cluster0.thqvugf.mongodb.net/socialmedia?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.thqvugf.mongodb.net/socialmedia?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
